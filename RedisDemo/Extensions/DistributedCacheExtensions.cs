bd100c2f108d8a6dfe362c9071c509b212ccbb1f
Srikanth Shettigar <SrikanthShettigar@gmail.com>
2021-01-06 10:44:00 -0500

    adding redis demo using c# to rep

---
+++ RedisDemo/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisDemo.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task setRecordAsync<T>(this IDistributedCache cache,
+            string recordID,
+            T data,
+            TimeSpan? absoluteExpireTime=null,
+            TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
+            options.SlidingExpiration = unusedExpireTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordID, jsonData, options);
+        }
+        public static async Task<T> getRecordAsync<T>(this IDistributedCache cache,
+            string recordID){
+            var jsonData = await cache.GetStringAsync(recordID);
+            if (jsonData is null)
+            {
+                return default(T);
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+
+
+    }
+}
